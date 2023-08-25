32b9e6615a4c057051234a3cdb62f5f7cd7f136d
Arpi Es <rp.eskandary@gmail.com>
2023-08-25 19:06:05 +0330

    [refactor] move theme folder to design system

--- library/designsystem/src/main/java/com/example/designsystem/theme/Color.kt
+++ library/designsystem/src/main/java/com/example/designsystem/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.example.composebreak.ui.theme
+package com.example.designsystem.theme
 
 import androidx.compose.ui.graphics.Color
 
--- library/designsystem/src/main/java/com/example/designsystem/theme/Type.kt
+++ library/designsystem/src/main/java/com/example/designsystem/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.example.composebreak.ui.theme
+package com.example.designsystem.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
