fd0183e36f4908f5300fd8d5e982a7ec37cb781c
Arpi Es <rp.eskandary@gmail.com>
2023-08-26 13:13:57 +0330

    [refactor] add Counter widget

---
+++ feature/countdown/src/main/java/com/example/countdown/component/Counter.kt
@@ -0,0 +1,41 @@
+package com.example.countdown.component
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import com.example.designsystem.ThemePreviews
+import com.example.designsystem.theme.ComposeBreakTheme
+
+@Composable
+fun Counter(
+    minutes: String,
+    seconds: String,
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        CounterText(text = minutes)
+        Text(text = " : ")
+        CounterText(text = seconds)
+    }
+
+}
+
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@ThemePreviews
+@Composable
+private fun CounterPreview() {
+
+    ComposeBreakTheme {
+        Surface {
+            Counter("32", "54")
+        }
+
+    }
+
+}
