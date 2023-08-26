bc66bc413cda980dce7deecab2126949673e3401
Arpi Es <rp.eskandary@gmail.com>
2023-08-26 16:58:43 +0330

    [refactor] add CounterController to screen

--- feature/countdown/src/main/java/com/example/countdown/CountdownScreen.kt
+++ feature/countdown/src/main/java/com/example/countdown/CountdownScreen.kt
@@ -2,12 +2,16 @@ package com.example.countdown
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.countdown.component.Counter
+import com.example.countdown.component.CounterController
 import com.example.designsystem.ThemePreviews
 import com.example.designsystem.theme.ComposeBreakTheme
 
@@ -15,14 +19,15 @@ import com.example.designsystem.theme.ComposeBreakTheme
 fun CountdownRoute(
     viewModel: CountdownViewModel,
 ) {
-    CountdownScreen("" , "")
+//    CountdownScreen("", "")
 }
 
 
 @Composable
 fun CountdownScreen(
-    minutes : String,
-    seconds: String,
+    counterState: CounterState,
+    onResetClicked: () -> Unit,
+    onPlayClicked: () -> Unit,
 ) {
 
     Column(
@@ -31,7 +36,13 @@ fun CountdownScreen(
         horizontalAlignment = Alignment.CenterHorizontally
 
     ) {
-        Counter(minutes = minutes, seconds = seconds)
+        Counter(minutes = "32", seconds = "45")
+        Spacer(modifier = Modifier.height(20.dp))
+        CounterController(
+            counterState = counterState,
+            onResetClicked = onResetClicked,
+            onPlayClicked = onPlayClicked
+        )
     }
 }
 
@@ -41,7 +52,11 @@ private fun CountdownScreenPreview() {
 
     ComposeBreakTheme {
         Surface {
-            CountdownScreen(minutes = "32", seconds = "54")
+            CountdownScreen(
+                counterState = CounterState.INITIAL,
+                onResetClicked = {},
+                onPlayClicked = {},
+            )
         }
 
     }
